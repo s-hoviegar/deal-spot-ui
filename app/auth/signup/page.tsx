182f0d34e4ca7e8aa5440c701b58eeec50bca625
Sajjad Hoviegar <sajjad@Sajjads-MBP.homenet.telecomitalia.it>
2024-08-22 02:08:08 +0200

    adding user fields to testing user sign up and integration with backend using server actions

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -1,15 +1,55 @@
+"use client";
+
 import { Button, Link, Stack, TextField } from "@mui/material";
 import NextLink from "next/link";
+import { useFormState } from "react-dom";
+import createUser from "./create-user";
 
 export default function Signup() {
+  const [state, formAction] = useFormState(createUser, { error: "" });
+
   return (
-    <Stack spacing={2} className="w-full max-w-xs">
-      <TextField label="Email" variant="outlined" type="email" />
-      <TextField label="Password" variant="outlined" type="password" />
-      <Button variant="contained">Signup</Button>
-      <Link component={NextLink} href="/auth/login" className="self-center">
-        Login
-      </Link>
-    </Stack>
+    <form action={formAction} className="w-full max-w-xs">
+      <Stack spacing={2}>
+        <TextField
+          name="name"
+          label="Name"
+          variant="outlined"
+          type="text"
+          helperText={state.error}
+          error={!!state.error}
+        />
+        <TextField
+          name="email"
+          label="Email"
+          variant="outlined"
+          type="email"
+          helperText={state.error}
+          error={!!state.error}
+        />
+        <TextField
+          name="password_hash"
+          label="Password"
+          variant="outlined"
+          type="password"
+          helperText={state.error}
+          error={!!state.error}
+        />
+        <TextField
+          name="last_login"
+          label="Last Login"
+          variant="outlined"
+          type="text"
+          helperText={state.error}
+          error={!!state.error}
+        />
+        <Button type="submit" variant="contained">
+          Signup
+        </Button>
+        <Link component={NextLink} href="/auth/login" className="self-center">
+          Login
+        </Link>
+      </Stack>
+    </form>
   );
 }
