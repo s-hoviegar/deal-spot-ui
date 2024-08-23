e9fa128d0cb898ea4cd7b558a600e17e9a66c5ee
Sajjad Hoviegar <sajjad@Sajjads-MBP.homenet.telecomitalia.it>
2024-08-23 10:39:18 +0200

    adding create user for signup

--- app/auth/signup/create-user.ts
+++ app/auth/signup/create-user.ts
@@ -5,9 +5,21 @@ import { getErrorMessage } from "@/app/util/errors";
 import { redirect } from "next/navigation";
 
 export default async function createUser(_prevState: any, formData: FormData) {
+  const filteredFormData = new FormData();
+
+  // List of fields you want to include in the form data
+  const allowedFields = ["name", "email", "password_hash", "last_login"];
+
+  // Convert formData entries to an array and then iterate
+  Array.from(formData.entries()).forEach(([key, value]) => {
+    if (allowedFields.includes(key)) {
+      filteredFormData.append(key, value);
+    }
+  });
+
   const res = await fetch(`${API_URL}/users`, {
     method: "POST",
-    body: formData,
+    body: filteredFormData,
   });
   const parsedRes = await res.json();
   if (!res.ok) {
